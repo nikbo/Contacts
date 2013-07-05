bec588a42f02d01529216e73cd387ff877e924a7
nikbo <nikita-bobrik@yandex.ru>
2013-07-05 15:01:02 +0300

    Add validation

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,7 +1,17 @@
+# coding: utf-8
 class Contact < ActiveRecord::Base
   attr_accessible :name, :email, :phone
-  validates :name, :presence => true
-  validates :email, :presence => true
-  validates :phone, :presence => true
+  validates :name, :presence => {:message => "не может быть пустым"}
+  validates :email, :presence => {:message => "не может быть пустым"},
+            :format => { :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i,
+                         :message => "неправильный, пожалуйста, введите корректный email" }
+  validates :phone,
+  :presence => {:message => "не может быть пустым"},
+  :numericality => { :only_integer => true,
+  :message => "должен быть в формате +XXXXXXXXX"},
+  :length => {
+      :minimum => 13,
+      :maximum => 13,
+      :message => "должен состоять из 12 цифр"}
 
 end
