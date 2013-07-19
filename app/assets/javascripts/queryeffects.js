e0ea3c691ac3598a4969ec721ab575a28b509b8c
nikbo <nikita-bobrik@yandex.ru>
2013-07-20 00:05:04 +0300

    Fix select of contact.

--- app/assets/javascripts/queryeffects.js
+++ app/assets/javascripts/queryeffects.js
@@ -1,19 +1,22 @@
 function select(form){
+    if (form.style.backgroundColor == "rgb(185, 185, 185)")
+        form.style.backgroundColor = "";
+    else{
     var elements = document.getElementsByClassName("table-r");
     for(var i=0; i<elements.length; i++){
-        elements[i].style.backgroundColor = "#ffffff";
+        elements[i].style.backgroundColor = "";
     }
     for(var i=0; i<elements.length; i++){
         if (elements[i].style.backgroundColor ==  "rgb(185, 185, 185)"){
 
-                elements[i].style.backgroundColor = "#ffffff";
+                elements[i].style.backgroundColor = "";
 
 
 
     }
     form.style.backgroundColor = "#b9b9b9";
 }
-
+    }
 }
 
 var links = [];
